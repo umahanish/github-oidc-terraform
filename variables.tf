4728d748affeb1f64015d5ad9472d5f119d757dd
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-16 21:40:03 +0300

    updated variable.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,29 @@
-variable "aws_region" { description = "AWS region" type = string }
-variable "vpc_cidr" { description = "VPC CIDR" type = string }
-variable "public_subnets" { type = list(string) }
-variable "private_subnets" { type = list(string) }
-variable "availability_zones" { type = list(string) }
-variable "tags" { type = map(string) }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+}
