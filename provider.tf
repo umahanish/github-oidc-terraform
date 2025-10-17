7afc4ffa776b42755b823dc0dd436ea84c47734b
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-17 18:55:33 +0300

    new vpc workflow

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "github-oidc-bucket-terraform"       # <-- replace with your S3 bucket name
-    key            = "aws-vpc/${terraform.workspace}/terraform.tfstate"
+   # key            = "aws-vpc/${terraform.workspace}/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-lock-table"            # <-- replace with your DynamoDB table name
     encrypt        = true
@@ -13,4 +13,8 @@ terraform {
       version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
