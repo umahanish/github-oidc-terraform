123a0bdef16a25e6540e2deec7e7ab7a53c51a75
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-17 16:06:18 +0300

    updated deploy

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,33 @@
-# Get existing VPC and Subnets from Terraform state
+#####################################################
+# EC2 Deployment using existing VPC from remote state
+#####################################################
+
+# --- Variables ---
+variable "environment" {
+  type    = string
+  default = "dev"
+  description = "Environment to deploy (dev/stage/prod)"
+}
+
+variable "key_pair_name" {
+  type        = string
+  description = "SSH key pair name for EC2 instance"
+}
+
+# --- Get existing VPC and Subnets from remote state ---
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket         = var.backend_bucket
-    key            = "aws-vpc/${var.environment}/terraform.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.dynamodb_table
+    bucket = "github-oidc-bucket-terraform"                # Your S3 bucket name
+    key    = "aws-vpc/${var.environment}/terraform.tfstate" # Remote VPC state path
+    region = "eu-west-2"                                   # VPC region
   }
 }
 
-# Create Security Group
+# --- Security Group ---
 resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
+  name        = "web-sg-${var.environment}"
   description = "Allow SSH and HTTP"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
@@ -41,13 +56,13 @@ resource "aws_security_group" "web_sg" {
   }
 
   tags = {
-    Name = "WebSecurityGroup"
+    Name = "WebSecurityGroup-${var.environment}"
   }
 }
 
-# Create EC2 instance in the first public subnet
+# --- EC2 Instance ---
 resource "aws_instance" "web" {
-  ami                         = "ami-046c2381f11878233" # Ubuntu 22.04 LTS (eu-west-2)
+  ami                         = "ami-046c2381f11878233"  # Ubuntu 22.04 LTS (eu-west-2)
   instance_type               = "t3.micro"
   subnet_id                   = data.terraform_remote_state.vpc.outputs.public_subnet_ids[0]
   associate_public_ip_address = true
@@ -64,10 +79,12 @@ resource "aws_instance" "web" {
               EOF
 
   tags = {
-    Name = "PublicEC2Instance"
+    Name = "PublicEC2Instance-${var.environment}"
   }
 }
-# Output the public IP of the EC2 instance
+
+# --- Output the public IP of the EC2 instance ---
 output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
+  value       = aws_instance.web.public_ip
+  description = "Public IP of the EC2 instance"
 }
