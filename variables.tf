b348ac8e85023f60d98d93efef0c08386a7c0120
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-17 17:01:39 +0300

    updated deploy

--- variables.tf
+++ variables.tf
@@ -3,10 +3,13 @@ variable "vpc_cidr" { default = "10.10.0.0/16" }
 variable "public_subnets" { default = ["10.10.1.0/24", "10.10.2.0/24"] }
 variable "private_subnets" { default = ["10.10.101.0/24", "10.10.102.0/24"] }
 variable "availability_zones" { default = ["eu-west-2a", "eu-west-2b"] }
+variable "environment" {
+  type    = string
+  default = "dev"
+}
 variable "tags" {
   default = {
-    Environment = "dev"
-    Project     = "aws-vpc-demo"
-    Owner       = "PlatformTeam"
+    Project = "aws-vpc-demo"
+    Owner   = "PlatformTeam"
   }
 }
