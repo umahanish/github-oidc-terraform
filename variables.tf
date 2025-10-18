e45d64079dd2c66749f46e58437cd3aa1f957c68
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-18 08:18:40 +0300

    updated deploy

--- variables.tf
+++ variables.tf
@@ -1,17 +1,66 @@
-variable "aws_region" { default = "eu-west-2" }
-variable "vpc_cidr" { default = "10.10.0.0/16" }
-variable "public_subnets" { default = ["10.10.1.0/24", "10.10.2.0/24"] }
-variable "private_subnets" { default = ["10.10.101.0/24", "10.10.102.0/24"] }
-variable "availability_zones" { default = ["eu-west-2a", "eu-west-2b"] }
-variable "environment" {
-  type    = string
-  default = "dev"
+# AWS Region
+variable "aws_region" {
+  type        = string
+  default     = "eu-west-2"
+  description = "The AWS region where resources will be deployed"
 }
-variable "tags" {
-  type = map(string)
-  default = {
-    Project = "aws-vpc-demo"
-    Owner   = "PlatformTeam"
+
+# VPC CIDR
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.10.0.0/16"
+  description = "The CIDR block for the VPC"
+}
+
+# Public Subnets
+variable "public_subnets" {
+  type        = list(string)
+  default     = ["10.10.1.0/24", "10.10.2.0/24"]
+  description = "List of public subnet CIDRs"
+
+  validation {
+    condition     = alltrue([for cidr in var.public_subnets : can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]{1,2}$", cidr))])
+    error_message = "Each public subnet must be a valid CIDR block (e.g., 10.10.1.0/24)"
   }
 }
 
+# Private Subnets
+variable "private_subnets" {
+  type        = list(string)
+  default     = ["10.10.101.0/24", "10.10.102.0/24"]
+  description = "List of private subnet CIDRs"
+
+  validation {
+    condition     = alltrue([for cidr in var.private_subnets : can(regex("^([0-9]{1,3}\\.){3}[0-9]{1,3}/[0-9]{1,2}$", cidr))])
+    error_message = "Each private subnet must be a valid CIDR block (e.g., 10.10.101.0/24)"
+  }
+}
+
+# Availability Zones
+variable "availability_zones" {
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b"]
+  description = "List of availability zones to deploy subnets"
+}
+
+# Environment
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Deployment environment"
+
+  validation {
+    condition     = contains(["dev", "qa", "prod"], var.environment)
+    error_message = "Environment must be one of 'dev', 'qa', or 'prod'."
+  }
+}
+
+# Tags
+variable "tags" {
+  type        = map(string)
+  default     = {
+    Project = "aws-vpc-demo"
+    Owner   = "PlatformTeam"
+  }
+  description = "Default tags to apply to all resources"
+}
