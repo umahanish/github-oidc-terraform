b677d5279ccabd5611301a4b635d8cc2ca56a336
umahanish@umagcpcognex.xyz <umahanish@umagcpcognex.xyz>
2025-10-18 07:42:06 +0300

    updated deploy

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "s3" {
+  backend "s3" { }
    # bucket         = "github-oidc-bucket-terraform"       # <-- replace with your S3 bucket name
    # key            = "aws-vpc/${terraform.workspace}/terraform.tfstate"
   #  region         = "eu-west-2"
    # dynamodb_table = "terraform-lock-table"            # <-- replace with your DynamoDB table name
   #  encrypt        = true
-  }
+ # }
 
   required_providers {
     aws = {
